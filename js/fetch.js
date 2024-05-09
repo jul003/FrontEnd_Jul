cfc388ea0b53c5581b843453f03f047b0ebc40e6
jul003 <dzulkiflifaiz11@gmail.com>
2024-05-10 00:44:14 +0700

    bodomoal

--- js/fetch.js
+++ js/fetch.js
@@ -1,11 +1,22 @@
 import { get } from "https://bukulapak.github.io/api/process.js";
-import { setInner } from "https://bukulapak.github.io/element/process.js";
+import { addInner } from "https://bukulapak.github.io/element/process.js";
+import { getRandomColor, getRandomColorName } from "https://bukulapak.github.io/image/process.js";
+import { isiTabel } from "../js/table.js";
 let urlAPI = "https://uts-jul2024-547e81327e27.herokuapp.com/bodomoal";
-get(urlAPI,isiTablePresensi);
-function isiTablePresensi(results){
-    console.log(results);
+get(urlAPI, isiTablePresensi);
+function isiTablePresensi(results) {
     results.forEach(isiRow);
 }
-function isiRow(value){
-    console.log(value)
+function isiRow(value) {
+    let content = 
+        `<tr>
+            <th scope="row">${value.id}</th>
+            <td>${value.merek}</td>
+            <td>${value.model}</td>
+            <td>${value.tipe}</td>
+            <td>${value.harga}</td>
+            <td>${value.warna}</td>
+            <td>${value.tahun_rilis}</td>
+        </tr>`;
+    addInner("iniTabel", content);
 }
---
+++ js/table.js
@@ -0,0 +1,78 @@
+export let isiTabel = 
+`
+<tr>
+                                      <th scope="col">ID</th>
+                                      <th scope="col">Merek</th>
+                                      <th scope="col">Model</th>
+                                      <th scope="col">Tipe</th>
+                                      <th scope="col">Harga</th>
+                                      <th scope="col">Warna</th>
+                                      <th scope="col">Tahun_Rilis</th>
+                                    </tr>
+                                  </thead>
+                                  <tbody>
+                                    <tr>
+                                      <th scope="row"></th>
+                                      <td></td>
+                                      <td></td>
+                                      <td></td>
+                                      <td></td>
+                                      <td></td>
+                                      <td></td>
+                                    </tr>
+                                    <tr>
+                                      <th scope="row"></th>
+                                      <td></td>
+                                      <td></td>
+                                      <td></td>
+                                      <td></td>
+                                      <td></td>
+                                      <td></td>
+                                    </tr>
+                                    <tr>
+                                      <th scope="row"></th>
+                                      <td></td>
+                                      <td></td>
+                                      <td></td>
+                                      <td></td>
+                                      <td></td>
+                                      <td></td>
+                                    </tr>
+                                    <tr>
+                                      <th scope="row"></th>
+                                      <td></td>
+                                      <td></td>
+                                      <td></td>
+                                      <td></td>
+                                      <td></td>
+                                      <td></td>
+                                    </tr>
+                                    <tr>
+                                      <th scope="row"></th>
+                                      <td></td>
+                                      <td></td>
+                                      <td></td>
+                                      <td></td>
+                                      <td></td>
+                                      <td></td>
+                                    </tr>
+                                    <tr>
+                                      <th scope="row"></th>
+                                      <td></td>
+                                      <td></td>
+                                      <td></td>
+                                      <td></td>
+                                      <td></td>
+                                      <td></td>
+                                    </tr>
+                                    <tr>
+                                      <th scope="row"></th>
+                                      <td></td>
+                                      <td></td>
+                                      <td></td>
+                                      <td></td>
+                                      <td></td>
+                                      <td></td>
+                    
+                                    </tr>
+                                    `
