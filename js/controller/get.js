0e430359014f577ed2da69cb141f5f468dde086c
hrisz <harissaefuloh@gmail.com>
2024-05-10 01:59:11 +0700

    done yeuh

--- js/controller/get.js
+++ js/controller/get.js
@@ -1,24 +1,32 @@
-import { addInner } from "https://bukulapak.github.io/element/process.js";
 import { isiTabel } from "../temp/table.js";
+
 export function isiTablePresensi(results) {
-    results.forEach(isiRow);
     clearTable();
+    results.forEach(isiRow);
 }
+
 function isiRow(value) {
     let content =
-        isiTabel.replace("ID", value._id)
+        isiTabel.replace("#ID", value._id)
             .replace("#Merek", value.merek)
             .replace("#Model", value.model)
             .replace("#Tipe", value.tipe)
             .replace("#Harga", value.harga)
             .replace("#Warna", value.warna)
-            .replace("#Tahun", value.tahun_rilis)
+            .replace("#Tahun", value.tahun_rilis);
 
-    let tableBody = document.querySelector('tbody');
+    const tableBody = document.querySelector('#iniTabel tbody');
     if (tableBody) {
-        let tableRow = tableBody.querySelector('tr');
-        if (tableRow) {
-            tableRow.innerHTML = content;
-        }
+        // Create a new row element
+        let newRow = document.createElement('tr');
+        newRow.innerHTML = content; // Set content to the new row
+        tableBody.appendChild(newRow); // Append the new row to the tbody
+    }
+}
+
+function clearTable() {
+    const tableBody = document.querySelector('#iniTabel tbody');
+    if (tableBody) {
+        tableBody.innerHTML = ''; // Clear the inner HTML of the tbody
     }
 }
--- js/temp/table.js
+++ js/temp/table.js
@@ -1,12 +1,14 @@
 export let isiTabel = 
 `
-<tr>
-<th scope="row">#ID</th>
-<td>#Merek</td>
-<td>#Model</td>
-<td>#Tipe</td>
-<td>#Harga</td>
-<td>#Warna</td>
-<td>#Tahun</td>
-</tr>
+                                  <tbody>
+                                    <tr>
+                                      <th scope="row">#ID</th>
+                                      <td>#Merek</td>
+                                      <td>#Model</td>
+                                      <td>#Tipe</td>
+                                      <td>#Harga</td>
+                                      <td>#Warna</td>
+                                      <td>#Tahun</td>
+                                    </tr>
+                                  </tbody>
 `
