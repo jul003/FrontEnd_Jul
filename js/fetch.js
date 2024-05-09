9ec526db754efa36bad03a094433d549f1052210
jul003 <dzulkiflifaiz11@gmail.com>
2024-05-09 22:42:28 +0700

    samsudin

---
+++ js/fetch.js
@@ -0,0 +1,11 @@
+import { get } from "https://bukulapak.github.io/api/process.js";
+import { setInner } from "https://bukulapak.github.io/element/process.js";
+let urlAPI = "https://uts-jul2024-547e81327e27.herokuapp.com/bodomoal";
+get(urlAPI,isiTablePresensi);
+function isiTablePresensi(results){
+    console.log(results);
+    results.forEach(isiRow);
+}
+function isiRow(value){
+    console.log(value)
+}
